7e94928e449007ae442b0aa031070350754fdf5b
velver <110679864+velvers@users.noreply.github.com>
2024-08-10 03:15:29 +0700

    Add files via upload

---
+++ catbox.py
@@ -0,0 +1,32 @@
+import requests
+import sys
+import pyperclip
+from plyer import notification
+
+def upload_to_catbox(file_path):
+    print(f"uploading file: {file_path}")  # debug shit
+    url = 'https://catbox.moe/user/api.php'
+    userhash = '8dfe2b50a968edff0dd6ff1cc'  # userhash go here
+    files = {'fileToUpload': open(file_path, 'rb')}
+    data = {'reqtype': 'fileupload', 'userhash': userhash}
+
+    response = requests.post(url, files=files, data=data)
+    if response.status_code == 200:
+        file_url = response.text.strip()
+        print(f"upload successful: {file_url}")  # debug shit
+        pyperclip.copy(file_url)
+        notification.notify(
+            title="catbox",
+            message=f"file uploaded successfully :3 {file_url}",
+            timeout=5  # duration in seconds
+        )
+    else:
+        print(f"couldnt upload file (fuck): {response.status_code} - {response.text}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("no file provided, provide a file as an argument")
+    else:
+        file_path = sys.argv[1]
+        print(f"received file path: {file_path}")  # debug shit
+        upload_to_catbox(file_path)
